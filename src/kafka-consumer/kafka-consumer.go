9307a4f59f50e5bf3143c651a2bb16fe50e52ac1
Tarun Vashisth <tarun.vashisth@target.com>
2018-01-13 20:04:34 +0530

    Add kafka consumer

---
+++ src/kafka-consumer/kafka-consumer.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	kafka "github.com/Shopify/sarama"
+)
+
+func main() {
+	consumer, err := kafka.NewConsumer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	messages, err := consumer.ConsumePartition("test-topic", 0, kafka.OffsetNewest)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Printing consumed messages....")
+	for i := 0; i < 10; i++ {
+		select {
+		case message := <-messages.Messages():
+			fmt.Println(message.Offset, string(message.Value))
+		case err := <-messages.Errors():
+			fmt.Println(err)
+		}
+	}
+
+	consumer.Close()
+}
